bc7545df8139512797c04cd91d813ab71978c604
Ruslan <enterpetrovruslan@gmail.com>
2017-05-17 13:32:42 +0300

    Variable support added

--- calculator10/CalculatorBrain.swift
+++ calculator10/CalculatorBrain.swift
@@ -9,24 +9,22 @@
 import Foundation
 
 struct CalculatorBrain {
-    private var accumulator: Double?
+    
     private var descriptionAccumulator = " "
     
-    public var description: String {
-        if pendingBinaryOperation == nil {
-            return descriptionAccumulator
-        } else {
-            return pendingBinaryOperation!.descriptionFunction(pendingBinaryOperation!.descriptionOperand,
-                                                               pendingBinaryOperation!.descriptionOperand != descriptionAccumulator ? descriptionAccumulator : " ")
-        }
+    private enum operationHistoryItem {
+        case operand(Double)
+        case operation(String)
+        case variable(String)
     }
     
+    private var operationHistory = [operationHistoryItem]()
     
     private enum Operation {
         case constant(Double)
         case nullaryOperation(() -> Double, String)
-        case unaryOperation((Double) -> Double, ((String) -> String)?)
-        case binaryOperation((Double, Double) -> Double, ((String, String) -> String)?)
+        case unaryOperation((Double) -> Double, ((String) -> String)?, ((Double) -> String?)?)
+        case binaryOperation((Double, Double) -> Double, ((String, String) -> String)?, ((Double, Double) -> String?)?)
         case equals
         
     }
@@ -35,98 +33,175 @@ struct CalculatorBrain {
         "π" : Operation.constant(Double.pi),
         "e" : Operation.constant(M_E),
         "rand" : Operation.nullaryOperation({ Double(arc4random()) / Double(UINT32_MAX)}, "rand(0..1)"),
-        "√" : Operation.unaryOperation(sqrt, nil),
-        "sin" : Operation.unaryOperation(sin, nil),
-        "cos" : Operation.unaryOperation(cos, nil),
-        "tan": Operation.unaryOperation(tan,nil),
-        "ln" : Operation.unaryOperation(log, nil),
-        "±" : Operation.unaryOperation({ -$0 }, nil),
-        "x⁻¹" : Operation.unaryOperation({1.0/$0}, {"(" + $0 + ")⁻¹"}),
-        "×" : Operation.binaryOperation(*, nil),
-        "÷" : Operation.binaryOperation(/, nil),
-        "+" : Operation.binaryOperation(+, nil),
-        "-" : Operation.binaryOperation(-, nil),
-        "xʸ" : Operation.binaryOperation(pow, { $0 + "^" + $1 }),
+        "√" : Operation.unaryOperation(sqrt, nil, { $0 < 0 ? "√ of negative number" : nil } ),
+        "sin" : Operation.unaryOperation(sin, nil, nil),
+        "cos" : Operation.unaryOperation(cos, nil, nil),
+        "tan": Operation.unaryOperation(tan,nil, nil),
+        "ln" : Operation.unaryOperation(log, nil, { $0 <= 0 ? "ln of non-positive number" : nil } ),
+        "±" : Operation.unaryOperation({ -$0 }, nil, nil),
+        "x⁻¹" : Operation.unaryOperation({1.0/$0}, {"(" + $0 + ")⁻¹"}, { $0 == 0 ? "Dividing by 0" : nil } ),
+        "х²" : Operation.unaryOperation({1.0/$0}, {"(" + $0 + ")²"}, nil),
+        "×" : Operation.binaryOperation(*, nil, nil),
+        "÷" : Operation.binaryOperation(/, nil, { $1 == 0 ? "Dividing by 0" : nil } ),
+        "+" : Operation.binaryOperation(+, nil, nil),
+        "-" : Operation.binaryOperation(-, nil, nil),
+        "xʸ" : Operation.binaryOperation(pow, { $0 + "^" + $1 }, nil),
         "=" : Operation.equals
     ]
     
-    mutating func superClear() {
-        accumulator = nil
-        pendingBinaryOperation = nil
-        descriptionAccumulator = " "
+    mutating func clear() {
+        operationHistory.removeAll()
     }
     
-    mutating func performOperation(_ symbol: String) {
-        if let operation = operations[symbol] {
-            switch operation {
-            case .constant(let value):
-                accumulator = value
-                descriptionAccumulator = symbol
-            case .nullaryOperation(let function, let descriptionValue):
-                accumulator = function()
-                descriptionAccumulator = descriptionValue
-            case .unaryOperation(let function, var descriptionFunction):
-                if accumulator != nil {
-                    accumulator = function(accumulator!)
-                    if descriptionFunction == nil {
-                        descriptionFunction = {symbol + "(" + $0 + ")"}
-                    }
-                    descriptionAccumulator = descriptionFunction!(descriptionAccumulator)
-                }
-            case .binaryOperation(let function, var descriptionFunction):
-                performPendingBinaryOperation()
-                if accumulator != nil {
-                    if descriptionFunction == nil {
-                        descriptionFunction = {$0 + symbol + $1}
-                    }
-                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!, descriptionFunction: descriptionFunction!, descriptionOperand: descriptionAccumulator)
-                    accumulator = nil
-                }
-            case .equals:
-                performPendingBinaryOperation()
-            }
+    mutating func undo() {
+        if !operationHistory.isEmpty {
+            operationHistory = Array(operationHistory.dropLast())
         }
     }
     
-    private mutating func performPendingBinaryOperation() {
-        if pendingBinaryOperation != nil && accumulator != nil {
-            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
-            descriptionAccumulator = pendingBinaryOperation!.performDescription(with: descriptionAccumulator)
-            pendingBinaryOperation = nil
-        }
-    }
-    
-    private var pendingBinaryOperation: PendingBinaryOperation?
-    
-    public var resultIsPending: Bool {
-        get {
-            return (pendingBinaryOperation != nil) ? true : false
-        }
-    }
-    
-    private struct PendingBinaryOperation {
+    struct PendingBinaryOperation {     //mevorizes first operand and operation while binary operation is pending (
         let function: (Double, Double) -> Double
         let firstOperand: Double
         var descriptionFunction: (String, String) -> String
         var descriptionOperand: String
+        var validator: ((Double, Double) -> String?)?
+        
         func perform(with secondOperand: Double) -> Double {
             return function(firstOperand, secondOperand)
         }
         func performDescription(with secondOperand: String) -> String {
             return descriptionFunction(descriptionOperand, secondOperand)
         }
-    }
-    
-    mutating func setOperand (_ operand: Double) {
-        accumulator = operand
-        if let value = accumulator {
-            descriptionAccumulator = String(value)
+        func validate(with secondOperand: Double) -> String? {
+            guard let validator = validator else { return nil }
+            return validator(firstOperand, secondOperand)
         }
+        
     }
     
+    mutating func setOperand (_ operand: Double) {  operationHistory.append(operationHistoryItem.operand(operand))  }
+    
+    mutating func setOperand (named variable: String) { operationHistory.append(operationHistoryItem.variable(variable))    }
+    
+    mutating func performOperation(_ symbol: String) {  operationHistory.append(operationHistoryItem.operation(symbol))  }
+    
+    func evaluate(using variables: Dictionary<String, Double>? = nil) -> (result: Double?, isPending: Bool, description: String, error: String?) {
+        
+        var cache: (accumulator: Double?, descriptionAccumulator: String?)
+        var error: String?
+        
+        var pendingBinaryOperation: PendingBinaryOperation?
+        
+        var description: String? {
+            if pendingBinaryOperation == nil {
+                return cache.descriptionAccumulator
+            } else {
+                return pendingBinaryOperation!.descriptionFunction(pendingBinaryOperation!.descriptionOperand,
+                                                                   cache.descriptionAccumulator ?? "")
+
+            }
+        }
+        
+        var result: Double? { return cache.accumulator }
+        
+        var resultIsPending: Bool { return pendingBinaryOperation != nil }
+        
+        // MARK: Nested functions evaluate
+        
+        func setOperand(_ operand: Double) {
+            cache.accumulator = operand
+            if let value = cache.accumulator {
+                cache.descriptionAccumulator = formatter.string(from: NSNumber(value: value)) ?? ""
+            }
+        }
+        
+        func setOperand(named variable: String) {
+            cache.accumulator = variables?[variable] ?? 0
+            cache.descriptionAccumulator = variable
+        }
+        
+        func performOperation(_ symbol: String) {
+            if let operation = operations[symbol] {
+                switch operation {
+                case .nullaryOperation(let function, let descriptionValue):
+                    cache = (function(), descriptionValue)
+                case .constant(let value):
+                    cache = (value, symbol)
+                case .unaryOperation(let function, var descriptionFunction, let validator):
+                    guard cache.accumulator != nil else { return }
+                    error = validator?(cache.accumulator!)
+                    cache.accumulator = function(cache.accumulator!)
+                    if descriptionFunction == nil {
+                        descriptionFunction = { symbol + "(" + $0 + ")" }
+                    }
+                    cache.descriptionAccumulator = descriptionFunction!(cache.descriptionAccumulator!)
+                case .binaryOperation(let function, var descriptionFunction, let validator):
+                    performPendingBinaryOperation()
+                    guard cache.accumulator != nil else { return }
+                    if descriptionFunction == nil {
+                        descriptionFunction = { $0 + symbol + $1 }
+                    }
+                    pendingBinaryOperation = PendingBinaryOperation(function: function,
+                                                                    firstOperand: cache.accumulator!,
+                                                                    descriptionFunction: descriptionFunction!,
+                                                                    descriptionOperand: cache.descriptionAccumulator!,
+                                                                    validator: validator)
+                    cache = (nil, nil)
+                case .equals:
+                    performPendingBinaryOperation()
+                }
+            }
+        }
+        
+        func performPendingBinaryOperation () {
+            guard pendingBinaryOperation != nil && cache.accumulator != nil else { return }
+            error = pendingBinaryOperation!.validate(with: cache.accumulator!)
+            cache.accumulator = pendingBinaryOperation!.perform(with: cache.accumulator!)
+            cache.descriptionAccumulator = pendingBinaryOperation?.performDescription(with: cache.descriptionAccumulator!)
+            pendingBinaryOperation = nil
+        }
+        
+        
+        // MARK: Body evaluate
+        
+        guard !operationHistory.isEmpty else { return (nil, false, " ", nil)}
+        for item in operationHistory {
+            switch item {
+            case .operand(let operand):
+                setOperand(operand)
+            case .variable(let operand):
+                setOperand(named: operand)
+            case .operation(let symbol):
+                performOperation(symbol)
+            }
+        }
+        return (result, resultIsPending, description ?? "", error)
+        
+    }
+
+    @available(iOS, deprecated, message: "No longer needed")
+    public var description: String {
+        return evaluate().description
+    }
+    
+    @available(iOS, deprecated, message: "No longer needed")
     public var result: Double?  {
-        get {
-            return accumulator
-        }
+        return evaluate().result
     }
+    
+    @available(iOS, deprecated, message: "No longer needed")
+    public var resultIsPending: Bool {
+        return evaluate().isPending
+    }
+    
+    
+    let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 6
+        formatter.notANumberSymbol = "Error"
+        formatter.groupingSeparator = " "
+        formatter.locale = Locale.current
+        return formatter
+    } ()
 }
--- calculator10/ViewController.swift
+++ calculator10/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
-    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var history: UILabel!
+    
+    @IBOutlet weak var displayM: UILabel!
     
     private var userIsInTheMiddleOfTyping = false
     
@@ -28,20 +30,42 @@ class ViewController: UIViewController {
             userIsInTheMiddleOfTyping = true
         }
     }
-    
-    @IBAction func clearAll(_ sender: UIButton) {
-        brain.superClear()
-        displayValue = 0
-        descriptionLabel.text = " "
+    @IBAction func setM(_ sender: UIButton) {
         userIsInTheMiddleOfTyping = false
+        let symbol = "M"
+        
+        variableValues[symbol] = displayValue
+        displayResult = brain.evaluate(using: variableValues)
     }
     
-    @IBAction func clearNumber(_ sender: UIButton) {
+    @IBAction func pushM(_ sender: UIButton) {
+        let symbol = "M"
+        brain.setOperand(named: symbol)
+        displayResult = brain.evaluate(using: variableValues)
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        brain.clear()
+        variableValues.removeAll()
+        displayResult = brain.evaluate()
+    }
+    
+    @IBAction func backspace(_ sender: UIButton) {
         if userIsInTheMiddleOfTyping {
-            displayValue = 0
-            userIsInTheMiddleOfTyping = false
+            guard !display.text!.isEmpty else { return }
+            display.text = String(display.text!.characters.dropLast())
+            if display.text!.isEmpty {
+                displayValue = 0
+                userIsInTheMiddleOfTyping = false
+                displayResult = brain.evaluate(using: variableValues)
+            }
+        } else {
+            brain.undo()
+            displayResult = brain.evaluate(using: variableValues)
         }
     }
+
+    
     @IBAction func removeLastDigit(_ sender: UIButton) {
         if userIsInTheMiddleOfTyping && !display.text!.isEmpty {
             display.text = String(display.text!.characters.dropLast())
@@ -61,21 +85,38 @@ class ViewController: UIViewController {
         }
     }
     
+    private var displayResult: (result: Double?, isPending: Bool, description: String, error: String?) = (nil, false, " ", nil) {
+        didSet {
+            switch displayResult {
+            case (nil, _, " ", nil):
+                displayValue = 0
+            case (let result, _, _, nil):
+                displayValue = result
+            case (_, _, _, let error):
+                display.text = error!
+            }
+            history.text = displayResult.description != " " ? displayResult.description + (displayResult.isPending ? " …" : " =") : " "
+            displayM.text = brain.formatter.string(from: NSNumber(value: variableValues["M"] ?? 0))
+            
+        }
+    }
+    
     private var brain = CalculatorBrain()
+    private var variableValues = [String: Double]()
     
     @IBAction func performOperation(_ sender: UIButton) {
-        if userIsInTheMiddleOfTyping, let value = displayValue {
-            brain.setOperand(value)
+        if userIsInTheMiddleOfTyping {
+            if let value = displayValue {
+                brain.setOperand(value)
+            }
             userIsInTheMiddleOfTyping = false
         }
         if let mathematicalSymbol = sender.currentTitle {
             brain.performOperation(mathematicalSymbol)
         }
-        if let result = brain.result {
-            displayValue = result
-        }
-        descriptionLabel.text = brain.description + (brain.resultIsPending ? "…" : "=")
+        displayResult = brain.evaluate(using: variableValues)
     }
+    
     
 }
 
